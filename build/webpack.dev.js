1d47bce4ce1a4e419f972d04c93a6cb462c419aa
杨栋 <1579489644@qq.com>
2019-04-29 17:42:52 +0800

    webpack 配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -9,12 +9,19 @@ const webpackcommonConfig = require('./webpack.common')
 module.exports = merge(webpackcommonConfig, {
   mode: 'development', // 不压缩代码，加快编译速度
   // devtool: 'source-map', // 提供源码映射文件调试使用
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: './dist',
     hot: true,
     open: true, // 自动打开浏览器
-    compress: true// 启动服务器压缩
+    compress: true, // 启动服务器压缩
+    host: '0.0.0.0', // 服务器外部可访问
+    overlay: {
+      errors: true
+    },
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
   },
   module: {
     rules: [{
