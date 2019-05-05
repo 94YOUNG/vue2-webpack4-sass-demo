feed5550f4306a7a7372b09cceb71532a2b732e3
杨栋 <1579489644@qq.com>
2019-05-05 17:57:38 +0800

    webpack 配置--公共代码分割

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,7 +2,7 @@
  *@desc
  *Created by yd on 2019-04-23
  */
-const merge = require('webpack-merge')
+const merge = require('webpack-merge')// 合并配置
 const webpack = require('webpack')
 const webpackcommonConfig = require('./webpack.common')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -34,5 +34,17 @@ module.exports = merge(webpackcommonConfig, {
   ],
   optimization: {
     // minimize: true,//压缩代码，效果和UglifyJSPlugin一样，production模式已经默认设置
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2,
+          maxInitialRequests: 5,
+          minSize: 0
+        }
+      },
+      chunks: 'all'
+    },
+    runtimeChunk: 'single'
   }
 })
